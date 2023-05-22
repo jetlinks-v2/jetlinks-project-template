caf044c9baee4a284b92cf607bf6b242aef83cd9
zhouhao <zh.sqy@qq.com>
2023-05-22 20:04:26 +0800

    feat(example): 增加join查询例子

--- modules/crud-examples/src/main/java/org/jetlinks/project/example/controller/ExampleController.java
+++ modules/crud-examples/src/main/java/org/jetlinks/project/example/controller/ExampleController.java
@@ -1,14 +1,28 @@
 package org.jetlinks.project.example.controller;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
+import org.hswebframework.ezorm.rdb.mapping.annotation.ColumnType;
+import org.hswebframework.ezorm.rdb.mapping.annotation.EnumCodec;
+import org.hswebframework.web.api.crud.entity.PagerResult;
+import org.hswebframework.web.api.crud.entity.QueryParamEntity;
 import org.hswebframework.web.authorization.annotation.Resource;
+import org.hswebframework.web.crud.query.QueryHelper;
 import org.hswebframework.web.crud.web.reactive.ReactiveServiceCrudController;
 import org.jetlinks.project.example.entity.ExampleEntity;
+import org.jetlinks.project.example.entity.ExtendedEntity;
+import org.jetlinks.project.example.enums.ExampleEnum;
 import org.jetlinks.project.example.service.ExampleService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import javax.persistence.Column;
+import java.sql.JDBCType;
 
 @RestController
 @RequestMapping("/example/crud")
@@ -20,5 +34,45 @@ public class ExampleController implements ReactiveServiceCrudController<ExampleE
 
     private final ExampleService service;
 
+    private final QueryHelper queryHelper;
 
+    @GetMapping("/_detail/_query")
+    public Mono<PagerResult<ExampleInfo>> joinExample(QueryParamEntity query) {
+        return queryHelper
+            .select(ExampleInfo.class)
+            .all(ExampleEntity.class)
+            .all(ExtendedEntity.class, ExampleInfo::setExt)
+            .from(ExampleEntity.class)
+            .leftJoin(ExtendedEntity.class, spec -> spec.is(ExtendedEntity::getId, ExampleEntity::getId))
+            .where(query)
+            .fetchPaged();
+    }
+
+    @GetMapping("/_detail/_query_native")
+    public Mono<PagerResult<ExampleInfo>> nativeJoinExample(QueryParamEntity query) {
+        return queryHelper
+            .select("select t.*,ext.* from example_crud t" +
+                        " left join example_crud_ext ext on ext.example_id = t.id",
+                    ExampleInfo::new)
+            .where(query)
+            .fetchPaged()
+            .doOnNext(c->{
+                System.out.println(c);
+            });
+    }
+
+
+    @Getter
+    @Setter
+    public static class ExampleInfo {
+        private String id;
+
+        private String name;
+
+        private ExampleEnum singleEnum;
+
+        private ExampleEnum[] multiEnum;
+
+        private ExtendedEntity ext;
+    }
 }
--- modules/crud-examples/src/test/java/org/jetlinks/project/example/controller/ExampleControllerTest.java
+++ modules/crud-examples/src/test/java/org/jetlinks/project/example/controller/ExampleControllerTest.java
@@ -1,5 +1,8 @@
 package org.jetlinks.project.example.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import org.hswebframework.web.api.crud.entity.PagerResult;
 import org.hswebframework.web.starter.jackson.CustomCodecsAutoConfiguration;
 import org.hswebframework.web.starter.jackson.CustomJackson2jsonEncoder;
 import org.jetlinks.pro.test.spring.TestJetLinksController;
@@ -8,7 +11,10 @@ import org.jetlinks.project.example.enums.ExampleEnum;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,16 +33,60 @@ class ExampleControllerTest extends TestJetLinksController {
             .expectStatus()
             .is2xxSuccessful();
 
-        client
+        List<ExampleEntity> entities = client
             .get()
-            .uri(builder-> builder
+            .uri(builder -> builder
                 .path("/example/crud/_query/no-paging")
-                .queryParam("where","multiEnum in enum1,enum3")
+                .queryParam("where", "multiEnum in enum1,enum3")
                 .build())
             .exchange()
             .expectBodyList(ExampleEntity.class)
-            .hasSize(1);
+            .returnResult()
+            .getResponseBody();
+        System.out.println(JSON.toJSONString(entities, SerializerFeature.PrettyFormat));
 
+    }
+
+    @Test
+    void testJoin() {
+        client
+            .post()
+            .uri("/example/crud")
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue("{\"name\": \"joinTest\",\"singleEnum\": \"enum1\",\"multiEnum\": [\"enum1\"]}")
+            .exchange()
+            .expectStatus()
+            .is2xxSuccessful();
+
+        {
+            String result = client
+                .get()
+                .uri(builder -> builder
+                    .path("/example/crud/_detail/_query")
+                    .queryParam("where", "name is joinTest")
+                    .build())
+                .exchange()
+                .expectBody(String.class)
+                .returnResult()
+                .getResponseBody();
+            assertNotNull(result);
+            System.out.println(JSON.toJSONString(JSON.parse(result), SerializerFeature.PrettyFormat));
+        }
+
+        {
+            String result = client
+                .get()
+                .uri(builder -> builder
+                    .path("/example/crud/_detail/_query_native")
+                    .queryParam("where", "name is joinTest")
+                    .build())
+                .exchange()
+                .expectBody(String.class)
+                .returnResult()
+                .getResponseBody();
+            assertNotNull(result);
+            System.out.println(JSON.toJSONString(JSON.parse(result), SerializerFeature.PrettyFormat));
+        }
     }
 
 }
