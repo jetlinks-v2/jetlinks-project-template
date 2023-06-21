544e8ba28d02ed5b42585ecedc5f6141d67b046c
zhouhao <zh.sqy@qq.com>
2023-06-21 16:22:22 +0800

    feat(脚手架): 增加拓展字段例子

---
+++ modules/examples/expand-property-example/src/main/java/org/jetlinks/project/example/UserDetailExt.java
@@ -0,0 +1,15 @@
+package org.jetlinks.project.example;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+import org.jetlinks.pro.auth.entity.UserDetail;
+
+@Getter
+@Setter
+public class UserDetailExt extends UserDetail {
+
+    @Schema(description = "企业名称")
+    private String corpName;
+
+}
