bb1017790ef5aae0c6d17219ea973c14055cea09
zhouhao <zh.sqy@qq.com>
2023-08-01 16:29:53 +0800

    test(脚手架): 修复单元测试错误

--- modules/examples/crud-examples/src/test/java/org/jetlinks/project/example/ExampleTestConfiguration.java
+++ modules/examples/crud-examples/src/test/java/org/jetlinks/project/example/ExampleTestConfiguration.java
@@ -1,11 +1,13 @@
 package org.jetlinks.project.example;
 
+import org.hswebframework.web.cache.configuration.ReactiveCacheManagerConfiguration;
 import org.hswebframework.web.starter.jackson.CustomCodecsAutoConfiguration;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
 @ImportAutoConfiguration({
-    CustomCodecsAutoConfiguration.class
+    CustomCodecsAutoConfiguration.class,
+    ReactiveCacheManagerConfiguration.class
 })
 @Configuration
 public class ExampleTestConfiguration {
